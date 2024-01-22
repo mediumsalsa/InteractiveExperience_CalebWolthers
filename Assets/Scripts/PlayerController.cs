2a33e1452c0d10a5edbadda7ca5d812ad02a67b1
Caleb Wolthers <w0473807@nscc.ca>
2024-01-22 12:09:13 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float movementSpeed = 1f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float vInput = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(hInput, vInput).normalized;
+
+        rb.position += movement * movementSpeed * Time.deltaTime;
+
+    }
+
+
+
+}
